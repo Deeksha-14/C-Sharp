36836c7c71be565b4a48e490096f6b2cb48c9842
DEEKSHA KUSHWAH <deekshakushwah0@gmail.com>
2023-08-09 16:38:38 +0530

    Add files via upload

---
+++ HRManagement/CompanyApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using emp;
+using HR;
+
+Console.WriteLine("Hello, World");
+
+
+//string deptNo, int empno, string fname, string lname, string em, string cNum
+Employee e1 = new HrManager("1", 101, "abc", "def", "abc@def", "12345");
+Console.WriteLine(e1);
